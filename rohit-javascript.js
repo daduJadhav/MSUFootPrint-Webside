d8eef4ef78d4c9a975d6a435c49f53aa576e076e
daduJadhav <87080275+daduJadhav@users.noreply.github.com>
2023-03-04 15:12:46 +0530

    commit 1

---
+++ rohit-javascript.js
@@ -0,0 +1,110 @@
+// var color = ["#e70634","#16eb03","#e70634","#16eb03"];
+// var i = 0;
+
+// ONE
+function seen1(){
+    document.getElementById('seen1').style.background="#4fbe58f7";
+    document.getElementById('seen1').textContent = "UN-SEEN";
+    
+}
+function seen1un(){
+    document.getElementById('seen1').style.background="#e70634";
+    document.getElementById('seen1').textContent = "SEEN";
+
+}
+
+// TWO
+
+function seen2(){
+    document.getElementById('seen2').style.background="#4fbe58f7";
+    document.getElementById('seen2').textContent = "UN-SEEN";
+
+}
+function seen2un(){
+    document.getElementById('seen2').style.background="#e70634";
+    document.getElementById('seen2').textContent = "SEEN";
+
+}
+
+
+// THRE
+
+function seen3(){
+    document.getElementById('seen3').style.background="#4fbe58f7";
+    document.getElementById('seen3').textContent = "UN-SEEN";
+
+}
+function seen3un(){
+    document.getElementById('seen3').style.background="#e70634";
+    document.getElementById('seen3').textContent = "SEEN";
+
+}
+
+//FOUR
+
+function seen4(){
+    document.getElementById('seen4').style.background="#4fbe58f7";
+    document.getElementById('seen4').textContent = "UN-SEEN";
+}
+function seen4un(){
+    document.getElementById('seen4').style.background="#e70634";
+    document.getElementById('seen4').textContent = "SEEN";
+
+}
+
+
+//FIVE
+
+
+function seen5(){
+    document.getElementById('seen5').style.background="#4fbe58f7";
+    document.getElementById('seen5').textContent = "UN-SEEN";
+
+}
+function seen5un(){
+    document.getElementById('seen5').style.background="#e70634";
+    document.getElementById('seen5').textContent = "SEEN";
+
+}
+
+
+//SIX
+
+function seen6(){
+    document.getElementById('seen6').style.background="#4fbe58f7";
+    document.getElementById('seen6').textContent = "UN-SEEN";
+
+}
+function seen6un(){
+    document.getElementById('seen6').style.background="#e70634";
+    document.getElementById('seen6').textContent = "SEEN";
+
+}
+
+
+//SEVEN
+
+function seen7(){
+    document.getElementById('seen7').style.background="#4fbe58f7";
+    document.getElementById('seen7').textContent = "UN-SEEN";
+
+}
+function seen7un(){
+    document.getElementById('seen7').style.background="#e70634";
+    document.getElementById('seen7').textContent = "SEEN";
+
+    
+}
+
+
+// Mark As readed
+
+function MAR(){
+    seen1();
+    seen2();
+    seen3();
+    seen4();
+    seen5();
+    seen6();
+    seen7();
+}
